20cae5f3b5f4c5a134803663f909a7b4b0f40ef1
fadlisabda1 <fadlisabda1@yahoo.com>
2020-07-16 20:05:43 +0700

    membuat class vertex

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
+//Graph Tidak Berarah Dan Berbobot Dengan Algoritma Kruskal
 #include <iostream>
 using namespace std;
+
+//vertex merupakan himpunan node atau titik pada sebuah graph
+//edge adalah garis yang menghubungkan tiap vertex pada sebuah graph
+//adjacent dua buah vertex berdekatan(adjacent) dan terhubung  dengan satu garis
+//cycle siklus atau sirkuit yaitu lintasan yang berawal dan berakhir pada simpul yang sama
+//weight bobot nilai yang dimiliki oleh sebuah lintasan(edge)antara dua buah vertex yang terhubung
+
+class vertex{
+    public:
+        char lab;//karakter nama dari vertex yang bersangkutan.
+        vertex *parent;//digunakan untuk menelusuri alamat vertex yang belum dilewati.
+        int rank;
+        vertex(char l){//nantinya akan digunakan ketika tiap vertex dibuat di dalam class graph.
+            lab=l;
+        }
+};
+
 int main(){
-    cout<<"hello world"<<endl;
+    
     return 0;
 }
